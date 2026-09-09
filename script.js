cf412b634877e86c9d1db1b80c24264d8181b47a
kahmeeah <kao8144@nyu.edu>
2026-09-09 00:01:18 -0400

    css animation! running from your Self .?

--- script.js
+++ script.js
@@ -5,3 +5,21 @@ youContained.addEventListener('mousemove', (event) => {
     youHovering.style.left = (event.clientX + 10) + 'px'
     youHovering.style.top = (event.clientY + 10) + 'px'
 })
+
+
+// redundant variable but i'm thinking currently abt the poetic potential of variables .. 
+const whydoYourun = document.querySelector('.you-container'); 
+
+whydoYourun.addEventListener('mouseenter', (event)=>{
+
+    const yourContainerWidth = whydoYourun.offsetWidth
+    const yourContainerHeight = whydoYourun.offsetHeight
+
+    const windowWidth = window.innerWidth;
+    const windowHeight = window.innerHeight;
+
+
+    whydoYourun.style.left = (Math.floor(Math.random() * (windowWidth - yourContainerWidth))) + 'px'
+    whydoYourun.style.top = (Math.floor(Math.random() * (windowHeight -  yourContainerHeight))) + 'px'
+
+})
