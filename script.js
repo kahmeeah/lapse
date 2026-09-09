791778cb1771785c66e8dbe0d6e4e0046d437488
kahmeeah <kao8144@nyu.edu>
2026-09-09 00:18:14 -0400

    random adj on hover!

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 const youContained = document.querySelector('.you-container');
 const youHovering = document.querySelector('.you-hover');
 
+const changingAdj = document.querySelector('#changingAdj');
+
+const words = [
+    'dallying','delaying', 'dilatory', 'dillydallying','dragging', 'goofing', 'idling',
+    'lagging', 'lazing', 'lingering', 'loafing', 'loitering', 'lolling', 'lollygagging',
+     'lounging', 'moping', 'procrastinatory', 'shilly-shallying', 'tarrying', 'trifling',
+     'waiting', 'wasting', 'timing', 'dawdling', 'laggard', 'procrastinating', 'inching',
+    'stalling', 'plodding']
+
 youContained.addEventListener('mousemove', (event) => {
     youHovering.style.left = (event.clientX + 10) + 'px'
     youHovering.style.top = (event.clientY + 10) + 'px'
+})
+
+youContained.addEventListener('mouseleave', (event) =>{
+    let randIndex = Math.floor(Math.random() * words.length);
+    changingAdj.textContent = words[randIndex]
+    console.log(changingAdj)
 })
 
 
